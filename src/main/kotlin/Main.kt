fccf6a4b7d4f32634cf19ddaf12e1b774efafc5a
stxrsh1ne <117750972+stxrsh1ne@users.noreply.github.com>
2023-10-04 22:01:57 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     val player = Player(12, "Player", 4)
-    val enemy = Enemy(8, 3, "Enemy", 15,)
+    val enemy = Enemy(8, 3, "Enemy", 15)
 
     val fight = Fight(player, enemy)
     fight.startFight()
